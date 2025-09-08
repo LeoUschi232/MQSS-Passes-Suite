fac34ed34fbf489c0b2f0d19049f95125858a8a9
Leonard Uscinowicz <ge78zic@mytum.de>
2025-09-08 13:38:32 +0200

    Refactored CMake back to AI as a subdir after all. The problem was torch and tensorflow but now both builds the script and clion build, build the AI toolbox.

--- AI/ai_pass_selector_tensorflow.cpp
+++ AI/ai_pass_selector_tensorflow.cpp
@@ -4,20 +4,20 @@
 #include <iostream>
 
 int main() {
-    tensorflow::Tensor t(tensorflow::DT_FLOAT, tensorflow::TensorShape({2, 3}));
+  tensorflow::Tensor t(tensorflow::DT_FLOAT, tensorflow::TensorShape({2, 3}));
 
-    // Fill with random values using the host CPU.
-    std::mt19937 gen(12345);
-    std::uniform_real_distribution dist(0.0f, 1.0f);
-    auto m = t.matrix<float>();
-    for (int i = 0; i < m.dimension(0); ++i) {
-        for (int j = 0; j < m.dimension(1); ++j) {
-            m(i, j) = dist(gen);
-        }
+  // Fill with random values using the host CPU.
+  std::mt19937 gen(12345);
+  std::uniform_real_distribution dist(0.0f, 1.0f);
+  auto m = t.matrix<float>();
+  for (int i = 0; i < m.dimension(0); ++i) {
+    for (int j = 0; j < m.dimension(1); ++j) {
+      m(i, j) = dist(gen);
     }
+  }
 
-    // Print a summary and the full values
-    std::cout << "Tensor summary: " << t.SummarizeValue(6) << "\n";
-    std::cout << "Tensor debug: " << t.DebugString() << "\n";
-    return 0;
+  // Print a summary and the full values
+  std::cout << "Tensor summary: " << t.SummarizeValue(6) << "\n";
+  std::cout << "Tensor debug: " << t.DebugString() << "\n";
+  return 0;
 }
