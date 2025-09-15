fba9ea42a00904a928bfd7c2e4472b1cccce3287
Leonard Uscinowicz <ge78zic@mytum.de>
2025-09-15 17:33:17 +0200

    lazy update

--- AI/include/Torch/A2C/instruction_based_a2c.hpp
+++ AI/include/Torch/A2C/instruction_based_a2c.hpp
@@ -5,47 +5,64 @@
 #include "Torch/agent_utils..hpp"
 
 namespace ai_pass_selector {
+    class InstructionBasedA2CAgent : public BaseA2CAgent {
+        InstructionBasedA2CAgent(
+            unsigned int max_qubits,
+            unsigned int max_instructions,
+            unsigned int max_depth,
+            int critic_optimizer_type,
+            int actor_optimizer_type,
+            double critic_learning_rate,
+            double actor_learning_rate,
+            unsigned int nr_parallel_environments,
+            torch::Device device);
+
+        std::unordered_map<std::string, std::string> train(
+            std::string dataset,
+            unsigned int episodes,
+            double discount_factor,
+            double gae_hyperparameter,
+            double entropy_coefficient) override;
+    };
 
 
+    /// IB = Instruction Based
+    /// DB = Instruction Based
+    /// FC = Fully Connected
+    /// CONV = Convolutional
+    /// LSM = Layer Size Maintaining
+    /// LSD = Layer Size Decreasing
+    class IB_FC_LSD_A2C final : public BaseA2CAgent {
+    public:
+        IB_FC_LSD_A2C(
+            unsigned int max_qubits,
+            unsigned int max_instructions,
+            unsigned int max_depth,
+            int critic_optimizer_type = OPTIMIZER_ADAM,
+            int actor_optimizer_type = OPTIMIZER_ADAM,
+            double critic_learning_rate = 0.005,
+            double actor_learning_rate = 0.001,
+            unsigned int nr_parallel_environments = 10,
+            torch::Device device = torch::kCPU);
 
-/// IB = Instruction Based
-/// DB = Instruction Based
-/// FC = Fully Connected
-/// CONV = Convolutional
-/// LSM = Layer Size Maintaining
-/// LSD = Layer Size Decreasing
-class IB_FC_LSD_A2C final : public BaseA2CAgent {
-public:
-  IB_FC_LSD_A2C(
-      unsigned int max_qubits,
-      unsigned int max_instructions,
-      unsigned int max_depth,
-      int critic_optimizer_type = OPTIMIZER_ADAM,
-      int actor_optimizer_type = OPTIMIZER_ADAM,
-      double critic_learning_rate = 0.005,
-      double actor_learning_rate = 0.001,
-      unsigned int nr_parallel_environments = 10,
-      torch::Device device = torch::kCPU);
+        std::string model_name() const override;
+    };
 
-  std::string model_name() const override;
-};
+    class IB_FC_LSM_A2C final : public BaseA2CAgent {
+    public:
+        IB_FC_LSM_A2C(
+            unsigned int max_qubits,
+            unsigned int max_instructions,
+            unsigned int max_depth,
+            int critic_optimizer_type = OPTIMIZER_ADAM,
+            int actor_optimizer_type = OPTIMIZER_ADAM,
+            double critic_learning_rate = 0.02,
+            double actor_learning_rate = 0.004,
+            unsigned int nr_parallel_environments = 5,
+            torch::Device device = torch::kCPU);
 
-class IB_FC_LSM_A2C final : public BaseA2CAgent {
-public:
-  IB_FC_LSM_A2C(
-      unsigned int max_qubits,
-      unsigned int max_instructions,
-      unsigned int max_depth,
-      int critic_optimizer_type = OPTIMIZER_ADAM,
-      int actor_optimizer_type = OPTIMIZER_ADAM,
-      double critic_learning_rate = 0.02,
-      double actor_learning_rate = 0.004,
-      unsigned int nr_parallel_environments = 5,
-      torch::Device device = torch::kCPU);
-
-  std::string model_name() const override;
-};
-
+        std::string model_name() const override;
+    };
 } // namespace ai_pass_selector
 
 #endif // INSTRUCTION_BASED_A2C_HPP
