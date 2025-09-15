389feae0f5ef422014970b36986ad5fc22a57808
Leonard Uscinowicz <ge78zic@mytum.de>
2025-09-15 11:22:00 +0200

    Optimized includes of cancellations.

--- lib/Passes/Cancellations/TdgTToId.cpp
+++ lib/Passes/Cancellations/TdgTToId.cpp
@@ -1,7 +1,6 @@
 #include "Passes/BaseMQSSPass.hpp"
 #include "Passes/Cancellations.hpp"
 #include "Support/CodeGen/Quake.hpp"
-#include "cudaq/Optimizer/Dialect/Quake/QuakeDialect.h"
 #include "cudaq/Optimizer/Dialect/Quake/QuakeOps.h"
 #include "cudaq/Support/Plugin.h"
 #include "mlir/IR/Threading.h"
@@ -10,13 +9,15 @@
 
 namespace mqss::opt {
 #define GEN_PASS_DEF_TDGTTOID
+
+// NOLINTNEXTLINE
 #include "Passes/Cancellations.h.inc"
 } // namespace mqss::opt
 
 using namespace mlir;
 
 namespace {
-void cancelTdgTToId(mlir::Operation *op) {
+void cancelTdgTToId(Operation *op) {
   auto t = dyn_cast_or_null<quake::TOp>(*op);
   if (!t || t.isAdj() || !t.getControls().empty() ||
       t.getTargets().size() != 1) {
@@ -32,28 +33,24 @@ void cancelTdgTToId(mlir::Operation *op) {
       prevGate.getTargets().size() != 1) {
     return;
   }
-  auto loc = prevGate.getLoc();
-  auto params = prevGate.getParameters();
-  auto ctrls = prevGate.getControls();
-  auto targs = prevGate.getTargets();
 
-  mlir::IRRewriter rewriter(t->getContext());
+  IRRewriter rewriter(t->getContext());
   rewriter.setInsertionPointAfter(t);
   rewriter.eraseOp(t);
   rewriter.eraseOp(prevGate);
 }
 
-class TdgTToId : public BaseMQSSPass<TdgTToId> {
+class TdgTToId final : public BaseMQSSPass<TdgTToId> {
 public:
   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(TdgTToId)
 
-  llvm::StringRef getArgument() const override { return "TdgTToId"; }
+  StringRef getArgument() const override { return "TdgTToId"; }
 
-  llvm::StringRef getDescription() const override {
+  StringRef getDescription() const override {
     return "Remove Tdg followed by T.";
   }
 
-  void operationsOnQuantumKernel(func::FuncOp kernel) override {
+  void operationsOnQuantumKernel(FuncOp kernel) override {
     kernel.walk([&](Operation *op) { cancelTdgTToId(op); });
   }
 };
--- lib/Passes/Cancellations/ZZToId.cpp
+++ lib/Passes/Cancellations/ZZToId.cpp
@@ -1,16 +1,15 @@
 #include "Passes/BaseMQSSPass.hpp"
 #include "Passes/Cancellations.hpp"
 #include "Support/Transforms/CancellationOperations.hpp"
-#include "cudaq/Optimizer/Dialect/Quake/QuakeDialect.h"
 #include "cudaq/Optimizer/Dialect/Quake/QuakeOps.h"
 #include "cudaq/Support/Plugin.h"
 #include "mlir/IR/Threading.h"
-#include "mlir/Rewrite/FrozenRewritePatternSet.h"
 #include "mlir/Transforms/DialectConversion.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 namespace mqss::opt {
 #define GEN_PASS_DEF_ZZTOID
+
+// NOLINTNEXTLINE
 #include "Passes/Cancellations.h.inc"
 } // namespace mqss::opt
 
@@ -18,17 +17,17 @@ using namespace mlir;
 using namespace mqss::support::transforms;
 
 namespace {
-class ZZToId : public BaseMQSSPass<ZZToId> {
+class ZZToId final : public BaseMQSSPass<ZZToId> {
 public:
   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(ZZToId)
 
-  llvm::StringRef getArgument() const override { return "ZZToId"; }
+  StringRef getArgument() const override { return "ZZToId"; }
 
-  llvm::StringRef getDescription() const override {
+  StringRef getDescription() const override {
     return "Remove consecutive Z gates.";
   }
 
-  void operationsOnQuantumKernel(func::FuncOp kernel) override {
+  void operationsOnQuantumKernel(FuncOp kernel) override {
     kernel.walk([&](Operation *op) {
       patternCancellation<quake::ZOp, quake::ZOp>(op, 0, 1, 0, 1);
     });
