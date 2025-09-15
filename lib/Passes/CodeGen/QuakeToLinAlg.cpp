b1752c6acc1f9644c3bbb60a037b3c4d64e347e5
Leonard Uscinowicz <ge78zic@mytum.de>
2025-09-15 11:27:48 +0200

    Optimized includes of code-gen.

--- lib/Passes/CodeGen/QuakeToLinAlg.cpp
+++ lib/Passes/CodeGen/QuakeToLinAlg.cpp
@@ -32,21 +32,13 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #include "Passes/CodeGen.hpp"
 #include "Support/CodeGen/Quake.hpp"
 #include "cudaq/Optimizer/Dialect/CC/CCOps.h"
-#include "cudaq/Optimizer/Dialect/CC/CCTypes.h"
-#include "cudaq/Optimizer/Dialect/Quake/QuakeDialect.h"
-#include "cudaq/Optimizer/Dialect/Quake/QuakeOps.h"
 #include "cudaq/Support/Plugin.h"
-#include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
-#include "mlir/Rewrite/FrozenRewritePatternSet.h"
 #include "mlir/Transforms/DialectConversion.h"
 
 #include "llvm/Support/Casting.h"
-#include "llvm/Support/raw_ostream.h"
 
-#include <iomanip>
 #include <regex>
-#include <unordered_map>
 
 using namespace mlir;
 using namespace mqss::support::quakeDialect;
@@ -54,73 +46,76 @@ using namespace mqss::interfaces;
 
 namespace {
 
-bool hasFunc(mlir::ModuleOp module, llvm::StringRef name) {
-  return static_cast<bool>(module.lookupSymbol<mlir::func::FuncOp>(name));
+[[maybe_unused]] bool hasFunc(ModuleOp module, StringRef name) {
+  return static_cast<bool>(module.lookupSymbol<FuncOp>(name));
 }
 
 class QuakeToLinAlg
-    : public PassWrapper<QuakeToLinAlg, OperationPass<ModuleOp>> {
+    : public PassWrapper<QuakeToLinAlg, OperationPass<ModuleOp> > {
 public:
   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(QuakeToLinAlg)
 
-  QuakeToLinAlg() {}
+  QuakeToLinAlg() {
+  }
 
-  llvm::StringRef getArgument() const override {
+  StringRef getArgument() const override {
     return "convert-quake-to-linalg";
   }
-  llvm::StringRef getDescription() const override {
+
+  StringRef getDescription() const override {
     return "Convert Quake to linalg Operations";
-  };
+  }
 
   void runOnOperation() override {
     ModuleOp module = getOperation();
     OpBuilder builder(module.getContext());
     auto context = &getContext();
-    context->getOrLoadDialect<mlir::tensor::TensorDialect>();
+    context->getOrLoadDialect<tensor::TensorDialect>();
     // Iterate through all functions
-    std::vector<func::FuncOp> quakeKernels;
-    for (auto func : module.getOps<func::FuncOp>()) {
+    std::vector<FuncOp> quakeKernels;
+    for (auto func : module.getOps<FuncOp>()) {
       // Check if the function has the "cudaq-kernel" attribute
       if (!func->hasAttr("cudaq-kernel"))
         continue;
-      llvm::StringRef functionName = func.getName();
+      StringRef functionName = func.getName();
       int numQubits = getNumberOfQubits(func);
-      mlir::Location loc = builder.getUnknownLoc();
+      Location loc = builder.getUnknownLoc();
       // Set insertion point inside the module
       builder.setInsertionPointToStart(module.getBody());
       // Create type of states of size  [2^numberQubits, 1]
-      auto elementType = mlir::ComplexType::get(builder.getF64Type());
-      mlir::RankedTensorType tensorType =
-          mlir::RankedTensorType::get({std::pow(2, numQubits)}, elementType);
+      auto elementType = ComplexType::get(builder.getF64Type());
+      RankedTensorType tensorType =
+          RankedTensorType::get({
+                                    static_cast<long>(std::pow(2, numQubits))},
+                                elementType);
       // Create the type of the matrices of each operation
       // The matrices has to be [2^numQubits, 2^numQubits]
-      auto shape = llvm::SmallVector<int64_t>{std::pow(2, numQubits),
-                                              std::pow(2, numQubits)};
-      auto matrixType = mlir::RankedTensorType::get(shape, elementType);
+      auto shape = SmallVector{
+          static_cast<long>(std::pow(2, numQubits)),
+          static_cast<long>(std::pow(2, numQubits))};
+      auto matrixType = RankedTensorType::get(shape, elementType);
       // the gpu function should be able to return a tensor of [2^numberQubits,
       // 1]
       auto funcType = builder.getFunctionType({}, {tensorType});
-      auto gpuFunc = builder.create<mlir::func::FuncOp>(
+      auto gpuFunc = builder.create<FuncOp>(
           loc, "mqss_gpu" + functionName.str(), funcType);
       // Agregar bloque de entrada vacío
-      mlir::Block *entryBlock = gpuFunc.addEntryBlock();
+      Block *entryBlock = gpuFunc.addEntryBlock();
       // Insert instructions
       builder.setInsertionPointToStart(entryBlock);
-      mlir::Value result = convertQuakeToLinAlg(
+      Value result = convertQuakeToLinAlg(
           module, func, builder, gpuFunc, tensorType, matrixType, numQubits);
-      builder.create<mlir::func::ReturnOp>(builder.getUnknownLoc(), result);
+      builder.create<func::ReturnOp>(builder.getUnknownLoc(), result);
       quakeKernels.push_back(func);
     }
     // deleting quake kernels after they are converted
     for (auto f : quakeKernels)
       f.erase();
   }
-
-private:
 };
 
 } // namespace
 
-std::unique_ptr<mlir::Pass> mqss::opt::createQuakeToLinAlgPass() {
+std::unique_ptr<Pass> mqss::opt::createQuakeToLinAlgPass() {
   return std::make_unique<QuakeToLinAlg>();
 }
