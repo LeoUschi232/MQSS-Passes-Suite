8d27b957839113cde74a3d05b8ece8b8b0143419
Leonard Uscinowicz <ge78zic@mytum.de>
2025-10-18 08:47:46 +0200

    Fixed bugs and syntax.

--- include/Passes/BaseMQSSPass.hpp
+++ include/Passes/BaseMQSSPass.hpp
@@ -30,9 +30,19 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 ******************************************************************************/
 #pragma once
 
+// Support includes
+#include "Support/mlir_utils.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+/// The usages of llvm functions must come before the QuakeOps header which
+/// expects them.
+using llvm::cast;
+using llvm::dyn_cast;
+using llvm::isa;
+////////////////////////////////////////////////////////////////////////////////
+
 #include "cudaq/Optimizer/Dialect/Quake/QuakeOps.h"
 #include "cudaq/Support/Plugin.h"
-#include "mlir/Rewrite/FrozenRewritePatternSet.h"
 
 using namespace mlir;
 
@@ -41,14 +51,14 @@ template <typename DerivedT>
 class BaseMQSSPass : public PassWrapper<DerivedT, OperationPass<ModuleOp>> {
 public:
   virtual void operationsOnQuantumKernel(
-      func::FuncOp kernel) = 0; // this has to be re-written by each pass
+      FuncOp kernel) = 0; // this has to be re-written by each pass
 private:
   std::tuple<SmallVector<Operation *, 16>, WalkResult>
   getQuakeKernels(ModuleOp module) {
     SmallVector<Operation *, 16> kernels;
     auto walkResult = module.walk([&kernels](Operation *op) {
       // Check if it is a quantum kernel
-      if (auto funcOp = dyn_cast<func::FuncOp>(op)) {
+      if (auto funcOp = dyn_cast<FuncOp>(op)) {
         if (funcOp->hasAttr(cudaq::entryPointAttrName)) {
           kernels.push_back(funcOp);
           return WalkResult::advance();
@@ -83,7 +93,7 @@ private:
       return;
     // Process kernels in parallel
     parallelForEach(module.getContext(), kernels, [this](Operation *kernel) {
-      if (auto funcOp = dyn_cast<func::FuncOp>(kernel)) {
+      if (auto funcOp = dyn_cast<FuncOp>(kernel)) {
         static_cast<DerivedT *>(this)->operationsOnQuantumKernel(funcOp);
       }
     });
