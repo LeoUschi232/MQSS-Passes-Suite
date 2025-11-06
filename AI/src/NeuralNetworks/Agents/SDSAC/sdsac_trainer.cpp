285523cede84edfe0e26ef6343bacea2175e51a5
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-06 16:13:50 +0100

    Fixed bugs and syntax.

--- AI/src/NeuralNetworks/Agents/SDSAC/sdsac_trainer.cpp
+++ AI/src/NeuralNetworks/Agents/SDSAC/sdsac_trainer.cpp
@@ -3,4 +3,10 @@
 namespace ai_pass_selector {
 
 
+std::unordered_map<std::string, std::string>
+train_sdsac(const std::unique_ptr<BaseSDSACAgent> &agent,
+            const std::string &dataset) {
+  return {};
+}
+
 } // namespace ai_pass_selector
