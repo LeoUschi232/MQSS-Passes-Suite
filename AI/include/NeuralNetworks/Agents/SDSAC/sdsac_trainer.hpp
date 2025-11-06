0ea2da0262bcd24618e4d46a474800ecc55c5e6c
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-06 16:13:06 +0100

    Fixed bugs and syntax.

--- AI/include/NeuralNetworks/Agents/SDSAC/sdsac_trainer.hpp
+++ AI/include/NeuralNetworks/Agents/SDSAC/sdsac_trainer.hpp
@@ -4,12 +4,14 @@
 // Agents includes
 #include "NeuralNetworks/Agents/SDSAC/base_sdsac_agent.hpp"
 
+// Torch
+
 namespace ai_pass_selector {
 struct SDSAC_EpisodeRollout {
   std::vector<torch::Tensor> observations; // [T+1, N_t, IRS]
   torch::Tensor actions;                   // [T]
   torch::Tensor rewards;                   // [T]
-  torch::Tensor entropies;                   // [T]
+  torch::Tensor entropies;                 // [T]
 };
 
 /**
@@ -19,8 +21,8 @@ struct SDSAC_EpisodeRollout {
  * @return
  */
 std::unordered_map<std::string, std::string>
-train_sdsac(const std::unique_ptr<BaseSACAgent> &agent,
-          const std::string &dataset);
+train_sdsac(const std::unique_ptr<BaseSDSACAgent> &agent,
+            const std::string &dataset);
 } // namespace ai_pass_selector
 
 #endif // SDSAC_TRAINER_HPP
