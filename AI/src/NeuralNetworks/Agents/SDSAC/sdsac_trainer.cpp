cdb04cb3db5d17fff567ccfb73120b092cc6e8a0
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-06 16:24:49 +0100

    Fixed bugs and syntax.

--- AI/src/NeuralNetworks/Agents/SDSAC/sdsac_trainer.cpp
+++ AI/src/NeuralNetworks/Agents/SDSAC/sdsac_trainer.cpp
@@ -1,15 +1,72 @@
 #include "NeuralNetworks/Agents/SDSAC/sdsac_trainer.hpp"
 
+// Environment includes
+#include "Environment/Wrappers/normalize_reward.hpp"
+
 // Utils includes
 #include "Utils/info_utils.hpp"
+#include "Utils/progress_bar.hpp"
+
+// Standard library includes
+#include <csignal>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+extern sig_atomic_t interrupted;
+extern void signal_handler(int signal);
 
 namespace ai_pass_selector {
-extern std::unordered_map<std::string, PassSelectorRuntimeParam>
-    GLOBAL_PARAMS;
+extern std::unordered_map<std::string, PassSelectorRuntimeParam> GLOBAL_PARAMS;
 
 std::unordered_map<std::string, std::string>
 train_sdsac(const std::unique_ptr<BaseSDSACAgent> &agent,
             const std::string &dataset) {
+  // Default values
+  unsigned int max_qubits = agent->getMaxQubits();
+  unsigned int nr_episodes = GLOBAL_PARAMS["nr_episodes"].to_int();
+  unsigned int max_steps_per_episode =
+      GLOBAL_PARAMS["max_steps_per_episode"].to_int();
+  unsigned int save_agent_every_ith_episode =
+      GLOBAL_PARAMS["save_agent_every_ith_episode"].to_int();
+  torch::Device device = GLOBAL_PARAMS["device"].to_device_type();
+  if (nr_episodes <= 0 || max_steps_per_episode <= 0) {
+    std::cerr << "Nothing to train." << std::endl;
+    return {};
+  }
+  auto optional_statistics = get_precomputed_dataset_statistics(dataset);
+  if (!optional_statistics.has_value()) {
+    std::cerr << "Dataset " + dataset + " doesn't have statistics for training."
+              << std::endl;
+    return {};
+  }
+  auto [qubits_cholesky_params, gates_weights] = optional_statistics.value();
+  NormalizeReward environment(QuantumCircuitEnvironment{max_qubits});
+  environment.register_randomizer_params(qubits_cholesky_params, gates_weights);
+  torch::TensorOptions options =
+      torch::TensorOptions().device(device).dtype(torch::kFloat32);
+  int64_t T = max_steps_per_episode;
+  std::cout << "Beginning training." << std::endl;
+  updateProgress(0, nr_episodes, /*display_message=*/"Beginning training");
+  SDSAC_EpisodeRollout rollout_old;
+  bool add_bootstrap_old = false;
+  double previous_episode_reward = 0.0;
+  unsigned int previous_nr_qubits = 0u;
+  unsigned int previous_nr_gates = 0u;
+  for (unsigned int episode_idx = 1; episode_idx <= nr_episodes;
+       episode_idx++) {
+    if (interrupted) {
+      break;
+    }
+    if (episode_idx % save_agent_every_ith_episode == 0) {
+      agent->save_model();
+      updateProgress(/*current=*/episode_idx, /*total=*/nr_episodes,
+                     /*display_message=*/"Saving Agent.");
+    }
+    updateProgress(/*current=*/episode_idx, /*total=*/nr_episodes,
+                   /*display_message=*/"Resetting Enviornment.");
+  }
+
   return {};
 }
 
