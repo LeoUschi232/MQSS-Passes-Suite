d0b9c94714b4a072448eb51ef4b07a8bc0143852
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-06 15:41:43 +0100

    Fixed bugs and syntax.

--- AI/include/NeuralNetworks/Agents/SDSAC/sac_agents.hpp
+++ AI/include/NeuralNetworks/Agents/SDSAC/sac_agents.hpp
@@ -1,11 +1,11 @@
-#ifndef SAC_AGENTS_HPP
-#define SAC_AGENTS_HPP
+#ifndef SDSAC_AGENTS_HPP
+#define SDSAC_AGENTS_HPP
 
 // Torch includes
 #include "NeuralNetworks/Agents/SAC/base_sac_agent.hpp"
 
-#define DECLARE_SAC_AGENT(ClassName)                                           \
-  class ClassName final : public BaseSACAgent {                                \
+#define DECLARE_SDSAC_AGENT(ClassName)                                           \
+  class ClassName final : public BaseSDSACAgent {                                \
   public:                                                                      \
     explicit ClassName(unsigned int max_qubits);                               \
     std::string agentName() const override;                                    \
@@ -20,11 +20,11 @@ namespace ai_pass_selector {
 /// HMPP = High Memory Plus Projection
 /// BMNP = Balanced Memory No Projection
 /// HYBRID = Hybrid of TCN and LSTM
-DECLARE_SAC_AGENT(SAC_TCN_RELU)
-DECLARE_SAC_AGENT(SAC_TCN_PRELU)
-DECLARE_SAC_AGENT(SAC_LSTM_HMPP)
-DECLARE_SAC_AGENT(SAC_LSTM_BMNP)
-DECLARE_SAC_AGENT(SAC_HYBRID)
+DECLARE_SDSAC_AGENT(SDSAC_TCN_RELU)
+DECLARE_SDSAC_AGENT(SDSAC_TCN_PRELU)
+DECLARE_SDSAC_AGENT(SDSAC_LSTM_HMPP)
+DECLARE_SDSAC_AGENT(SDSAC_LSTM_BMNP)
+DECLARE_SDSAC_AGENT(SDSAC_HYBRID)
 } // namespace ai_pass_selector
 
-#endif // SAC_AGENTS_HPP
+#endif // SDSAC_AGENTS_HPP
