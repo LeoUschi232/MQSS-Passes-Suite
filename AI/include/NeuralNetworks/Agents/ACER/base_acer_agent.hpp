1dcccc2a0c742f74a74c74371662c91010569938
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-12 16:45:26 +0100

    Fixed bugs and syntax.

--- AI/include/NeuralNetworks/Agents/ACER/base_acer_agent.hpp
+++ AI/include/NeuralNetworks/Agents/ACER/base_acer_agent.hpp
@@ -38,12 +38,30 @@ public:
   BaseACERAgent &operator=(BaseACERAgent &&other) noexcept = delete;
 
   //////////////////////////////////////////////////////////////////////////////
+  /// ACER override for disabling methods
+  bool initialize(const torch::nn::Sequential &actor,
+                  const torch::nn::Sequential &critic) override;
+  void update_parameters(const torch::Tensor &actor_loss,
+                         const torch::Tensor &critic_loss) const override;
+  std::pair<torch::Tensor, torch::Tensor>
+  forward(const torch::Tensor &observation) override;
+  torch::Tensor get_value(const torch::Tensor &observation) override;
+  std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor>
+  select_action(const torch::Tensor &observation) override;
+  //////////////////////////////////////////////////////////////////////////////
   /// ACER standard methods
   /**
    * @param observation
    * @return
    */
   torch::Tensor get_value(const torch::Tensor &observation) override;
+
+  /**
+   * @param observation
+   * @return [action_probs, state_value]
+   */
+  std::tuple<torch::Tensor, torch::Tensor, torch::Tensor>
+  acer_forward(const torch::Tensor &observation);
 
   /**
    *
