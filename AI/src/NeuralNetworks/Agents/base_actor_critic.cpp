54327f903cf0fca9d7b55208ac7ffa00bc451df7
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-12 17:12:29 +0100

    Fixed bugs and syntax.

--- AI/src/NeuralNetworks/Agents/base_actor_critic.cpp
+++ AI/src/NeuralNetworks/Agents/base_actor_critic.cpp
@@ -18,7 +18,6 @@
 #include <cmath>
 #include <memory>
 #include <tuple>
-#include <utility>
 
 namespace ai_pass_selector {
 extern std::unordered_map<std::string, PassSelectorRuntimeParam> GLOBAL_PARAMS;
@@ -36,83 +35,6 @@ BaseActorCritic::BaseActorCritic(unsigned int max_qubits)
   this->discount_factor = GLOBAL_PARAMS["discount_factor"].to_double();
   this->gae_hyperparameter = GLOBAL_PARAMS["gae_hyperparameter"].to_double();
   this->entropy_coefficient = GLOBAL_PARAMS["entropy_coefficient"].to_double();
-}
-
-bool BaseActorCritic::initialize(const torch::nn::Sequential &actor,
-                                 const torch::nn::Sequential &critic) {
-  try {
-    this->actor = actor;
-    this->critic = critic;
-    // Load the model before putting it to the device to avoid device
-    // scheduling issus.
-    this->load_model();
-    this->register_module("critic", this->critic);
-    this->register_module("actor", this->actor);
-    this->critic->to(this->device);
-    this->actor->to(this->device);
-    this->critic_optimizer = std::shared_ptr(
-        std::move(makeOptimizer(this->critic_optimizer_type, this->critic,
-                                this->critic_learning_rate)));
-    this->actor_optimizer = std::shared_ptr(std::move(makeOptimizer(
-        this->actor_optimizer_type, this->actor, this->actor_learning_rate)));
-  } catch (const std::runtime_error &e) {
-    std::cerr << e.what() << std::endl;
-    return false;
-  }
-  return true;
-}
-
-std::pair<torch::Tensor, torch::Tensor>
-BaseActorCritic::forward(const torch::Tensor &observation) {
-  torch::Tensor x = observation.to(this->device).to(torch::kFloat32);
-  // Do NOT reshape/flatten here.
-  // Let the models handle shapes.
-  return {this->actor->forward(x), this->critic->forward(x)};
-}
-
-torch::Tensor BaseActorCritic::get_value(const torch::Tensor &observation) {
-  return this->critic->forward(
-      observation.to(this->device).to(torch::kFloat32));
-}
-
-std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor>
-BaseActorCritic::select_action(const torch::Tensor &observation) {
-  auto [action_probs, state_value] = this->forward(observation);
-
-  // Multinomial selects num_samples=1 indices per row for the given matrix,
-  // using the values in the row as weights.
-  // action_probs ~ [NR_PASSES]
-  // X.multinomial(num_samples=1) ~ [1]
-  // X.squeeze(dim=-1) ~ []
-  const torch::Tensor action_index_unsqueezed =
-      action_probs.multinomial(/*num_samples=*/1);
-  const torch::Tensor action_index = action_index_unsqueezed.squeeze(-1);
-
-  // For advantage compute log π(a_t|s_t) for the sampled actions.
-  // Gather extracts the values at specified indexes along the specified axis.
-  // Parameter indexes must have the same nr of axes as the input tensor, here
-  // each has 2 axes.
-  // unsqueezed_log_action_probs ~ [NR_PASSES]
-  // X.gather(dim=-1, indexes=action_indexes_unsqueezed) ~ [1]
-  // X.squeeze(dim=-1) ~ []
-  const torch::Tensor unsqueezed_log_action_probs = action_probs.log();
-  const torch::Tensor log_action_prob =
-      unsqueezed_log_action_probs
-          .gather(/*dim=*/-1, /*indexes=*/action_index_unsqueezed)
-          .squeeze(-1);
-
-  // Entropy formula H = -sum_{x}(p(x)*log(p(x)))
-  // action_probs * log_action_probs ~ [NR_PASSES]
-  // -X.sum(dim=-1) ~ [1]
-  // X.squeeze(dim=-1) ~ []
-  const torch::Tensor entropy =
-      -(action_probs * unsqueezed_log_action_probs).sum(/*dim=*/-1).squeeze(-1);
-  return {
-      action_index,    // Shape []
-      log_action_prob, // Shape []
-      state_value,     // Shape []
-      entropy          // Shape []
-  };
 }
 
 unsigned int
