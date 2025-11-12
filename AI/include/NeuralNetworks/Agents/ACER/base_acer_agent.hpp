c8493642f93c66efd731f43c71c77f50668beba3
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-12 13:39:32 +0100

    Fixed bugs and syntax.

--- AI/include/NeuralNetworks/Agents/ACER/base_acer_agent.hpp
+++ AI/include/NeuralNetworks/Agents/ACER/base_acer_agent.hpp
@@ -1,6 +1,47 @@
 #ifndef BASE_ACER_AGENT_HPP
 #define BASE_ACER_AGENT_HPP
 
-namespace ai_pass_selector {} // namespace ai_pass_selector
+// Torch includes
+#include "torch/torch.h"
+
+// Neural-Networks includes
+#include "NeuralNetworks/Agents/base_actor_critic.hpp"
+
+// Standard library includes
+#include <memory>
+
+namespace fs = std::filesystem;
+
+namespace ai_pass_selector {
+enum class OptimizerType : int;
+
+class BaseACERAgent : public BaseActorCritic {
+protected:
+  /// ACER specific attributes
+  double acer_retrace_clip_c = 0.0;
+  double acer_trust_region_delta = 0.0;
+
+public:
+  /// Constructors
+  explicit BaseACERAgent(unsigned int max_qubits);
+
+  /// Destructor
+	~BaseACERAgent() override = default;
+
+  /// Copy and move constructors and assignment operators
+  BaseACERAgent(const BaseACERAgent &other) noexcept = delete;
+
+  BaseACERAgent(BaseACERAgent &&other) noexcept = default;
+
+  BaseACERAgent &operator=(const BaseACERAgent &other) noexcept = delete;
+
+  BaseACERAgent &operator=(BaseACERAgent &&other) noexcept = delete;
+
+  //////////////////////////////////////////////////////////////////////////////
+  /// ACER standard methods
+
+  //////////////////////////////////////////////////////////////////////////////
+}
+} // namespace ai_pass_selector
 
 #endif // BASE_ACER_AGENT_HPP
--- AI/src/NeuralNetworks/Agents/ACER/base_acer_agent.cpp
+++ AI/src/NeuralNetworks/Agents/ACER/base_acer_agent.cpp
@@ -1,3 +1,8 @@
 #include "NeuralNetworks/Agents/ACER/base_acer_agent.hpp"
 
-namespace ai_pass_selector {} // namespace ai_pass_selector
+namespace ai_pass_selector {
+
+
+
+
+} // namespace ai_pass_selector
