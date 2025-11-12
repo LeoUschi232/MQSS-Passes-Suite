f31ea4cfb330797bf733ffb602d92f65d37a9021
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-12 14:18:16 +0100

    Fixed bugs and syntax.

--- AI/include/NeuralNetworks/Agents/ACER/acer_trainer.hpp
+++ AI/include/NeuralNetworks/Agents/ACER/acer_trainer.hpp
@@ -1,6 +1,31 @@
 #ifndef ACER_TRAINER_HPP
 #define ACER_TRAINER_HPP
 
-namespace ai_pass_selector {} // namespace ai_pass_selector
+// Agents includes
+#include "NeuralNetworks/Agents/ACER/base_acer_agent.hpp"
+
+namespace ai_pass_selector {
+
+struct ACER_EpisodeRollout {
+  std::vector<torch::Tensor> observations; // [T+1, N_t, IRS]
+  torch::Tensor actions;                   // [T]
+  torch::Tensor log_action_probs;          // [T]
+  torch::Tensor rewards;                   // [T]
+};
+
+
+/**
+ *
+ * @param agent
+ * @param dataset
+ * @return
+ */
+std::unordered_map<std::string, std::string>
+train_ppo(const std::unique_ptr<BasePPOAgent> &agent,
+          const std::string &dataset);
+
+
+
+} // namespace ai_pass_selector
 
 #endif // ACER_TRAINER_HPP
