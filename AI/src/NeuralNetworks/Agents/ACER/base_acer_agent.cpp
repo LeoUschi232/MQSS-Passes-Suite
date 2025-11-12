1607ef445a2ed2867becdb81b7b28bde49739832
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-12 13:53:28 +0100

    Fixed bugs and syntax.

--- AI/src/NeuralNetworks/Agents/ACER/base_acer_agent.cpp
+++ AI/src/NeuralNetworks/Agents/ACER/base_acer_agent.cpp
@@ -1,14 +1,18 @@
 #include "NeuralNetworks/Agents/ACER/base_acer_agent.hpp"
 
+// Neural-Networks includes
+#include "NeuralNetworks/Agents/agent_utils.hpp"
+
+// Utils includes
+#include "Utils/info_utils.hpp"
+
 namespace ai_pass_selector {
 extern std::unordered_map<std::string, PassSelectorRuntimeParam> GLOBAL_PARAMS;
 BaseACERAgent::BaseACERAgent(unsigned int max_qubits)
     : BaseActorCritic(max_qubits) {
-  double ppo_epsilon = GLOBAL_PARAMS["ppo_epsilon"].to_double();
-  this->acer_retrace_clip_c = 1.0 - ppo_epsilon;
-  this->acer_trust_region_delta = 1.0 + ppo_epsilon;
+  this->acer_retrace_clip_c = GLOBAL_PARAMS["acer_retrace_clip_c"].to_double();
+  this->acer_trust_region_delta =
+      GLOBAL_PARAMS["acer_trust_region_delta"].to_double();
 }
-
-
 
 } // namespace ai_pass_selector
