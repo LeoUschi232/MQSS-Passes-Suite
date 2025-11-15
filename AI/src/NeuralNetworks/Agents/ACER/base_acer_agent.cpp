7e7027c1832883effc80b911e91e480e4415f3ff
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-15 19:32:06 +0100

    Fixed bugs and syntax.

--- AI/src/NeuralNetworks/Agents/ACER/base_acer_agent.cpp
+++ AI/src/NeuralNetworks/Agents/ACER/base_acer_agent.cpp
@@ -111,6 +111,9 @@ void BaseACERAgent::compute_losses_and_accumulate_gradients(
     // average policy, but we are detaching it here again just to be sure.
     torch::Tensor quantity_k =
         this->compute_KL_divergence(policies_avg[i].detach(), policies_main[i]);
+    torch::Tensor actor_loss = quantity_g - torch::max(
+      torch::tensor(0.0,  GLOBAL_TENSOR_OPTIONS), (quantity_k.dot(quantity_g) -this->acer_trust_region_delta)
+      )
 
     ////////////////////////////////////////////////////////////////////////////
     actor_loss.backward();
