bfaf0d15ad327a37d4e21cb818bdce736434c361
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-15 18:19:01 +0100

    Fixed bugs and syntax.

--- AI/src/NeuralNetworks/Agents/ACER/base_acer_agent.cpp
+++ AI/src/NeuralNetworks/Agents/ACER/base_acer_agent.cpp
@@ -101,6 +101,17 @@ void BaseACERAgent::update_assuming_gradients_are_computed() {
   this->actor_optimizer->step();
   this->critic_optimizer->step();
 
+  {
+    torch::NoGradGuard no_grad_guard;
+    for (const auto &pair : this->critic->named_parameters(/*recurse=*/true)) {
+      const std::string &name = pair.key();
+      torch::Tensor param_main = pair.value();
+      torch::Tensor param_avg =
+          this->critic_Q1_avg->named_parameters(/*recurse=*/true)[name];
+      param_avg.mul_(1.0 - this->sdsac_smoothing_tau);
+      param_avg.add_(this->sdsac_smoothing_tau * param_main);
+    }
+  }
 }
 
 unsigned int
