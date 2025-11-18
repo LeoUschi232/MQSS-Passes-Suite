4e8bc63542f1b9da7d6f4d50e48d693c9ff3f000
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-18 10:27:58 +0100

    Removed TCN full network with PReLU.

--- AI/include/NeuralNetworks/tcn_network.hpp
+++ AI/include/NeuralNetworks/tcn_network.hpp
@@ -1,5 +1,5 @@
-#ifndef TCN_RESIDUAL_BLOCK_HPP
-#define TCN_RESIDUAL_BLOCK_HPP
+#ifndef TCN_NETWORK_HPP
+#define TCN_NETWORK_HPP
 
 // Torch includes
 #include "torch/torch.h"
@@ -9,12 +9,20 @@ class TCNResidualBlock : public torch::nn::Module {
 protected:
   torch::nn::Sequential convolutional_block{nullptr};
   torch::nn::Sequential layer_norm{nullptr};
-
 public:
   TCNResidualBlock(unsigned int in_channels, unsigned int out_channels,
                    unsigned int kernel_size, unsigned int dilation,
                    double dropout = 0.2);
   torch::Tensor forward(const torch::Tensor &x);
 };
+class TCNFullNetwork : public torch::nn::Module {
+protected:
+  torch::nn::Sequential tcn_sequence{nullptr};
+public:
+  explicit TCNFullNetwork(unsigned int nr_channels,
+                          unsigned int nr_residual_blocks,
+                          unsigned int kernel_size, double dropout = 0.2);
+  torch::Tensor forward(const torch::Tensor &x);
+};
 } // namespace ai_pass_selector
-#endif // TCN_RESIDUAL_BLOCK_HPP
+#endif // TCN_NETWORK_HPP
--- AI/src/NeuralNetworks/tcn_network.cpp
+++ AI/src/NeuralNetworks/tcn_network.cpp
@@ -1,4 +1,4 @@
-#include "NeuralNetworks/tcn_residual_blocks.hpp"
+#include "NeuralNetworks/tcn_network.hpp"
 
 // Neural-Network includes
 #include "NeuralNetworks/layers_and_wrappers.hpp"
@@ -39,5 +39,24 @@ TCNResidualBlock::TCNResidualBlock(unsigned int in_channels,
 
 torch::Tensor TCNResidualBlock::forward(const torch::Tensor &x) {
   return this->layer_norm->forward(this->convolutional_block->forward(x) + x);
+}
+TCNFullNetwork::TCNFullNetwork(unsigned int nr_channels,
+                               unsigned int nr_residual_blocks,
+                               unsigned int kernel_size, double dropout)
+    : tcn_sequence(torch::nn::Sequential()) {
+  if (kernel_size % 2 == 0) {
+    throw std::invalid_argument("Kernel size in TCNResidualBlock must be odd.");
+  }
+  unsigned int dilation = 1u;
+  for (unsigned i = 0u; i < nr_residual_blocks; i++) {
+    this->tcn_sequence->push_back(TCNResidualBlock(
+        /*in_channels=*/nr_channels, /*out_channels=*/nr_channels, kernel_size,
+        dilation, dropout));
+    dilation <<= 1u;
+  }
+  this->register_module("tcn_sequence", this->tcn_sequence);
+}
+torch::Tensor TCNFullNetwork::forward(const torch::Tensor &x) {
+  return this->tcn_sequence->forward(x);
 }
 } // namespace ai_pass_selector
