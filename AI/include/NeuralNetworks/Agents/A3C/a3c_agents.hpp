dc25b367cb15dc25585db6d89bbfba022b0b3edf
Leonard Uscinowicz <ge78zic@mytum.de>
2025-11-18 10:52:33 +0100

    Adapeted A2C to new agent architectures.

--- AI/include/NeuralNetworks/Agents/A3C/a3c_agents.hpp
+++ AI/include/NeuralNetworks/Agents/A3C/a3c_agents.hpp
@@ -16,24 +16,14 @@ namespace ai_pass_selector {
 /// A2C = Advantage Actor-Critic
 /// A3C = Asynchronous Advantage Actor-Critic
 /// TCN = Temporal Convolutional Network
-/// RELU = Activation Functions are set to ReLU
-/// PRELU = Activation Functions are set to PReLU
 /// LSTM = Long Short-Term Memory
-/// HMPP = High Memory Plus Projection
-/// BMNP = Balanced Memory No Projection
 /// HYBRID = Hybrid of TCN and LSTM
-// a3c-mq28-tcnrelu Nr trainable parameters: 934434
-// a3c-mq130-tcnrelu Nr trainable parameters: 10845966
-DECLARE_A3C_AGENT(A3C_TCN_RELU)
-// a3c-mq28-tcnprelu Nr trainable parameters: 934494
-// a3c-mq130-tcnprelu Nr trainable parameters: 10846038
-DECLARE_A3C_AGENT(A3C_TCN_PRELU)
-// a3c-mq28-lstmhmpp Nr trainable parameters: 1057986
-// a3c-mq130-lstmhmpp Nr trainable parameters: 10160466
-DECLARE_A3C_AGENT(A3C_LSTM_HMPP)
-// a3c-mq28-lstmbmnp Nr trainable parameters: 1147266
-// a3c-mq130-lstmbmnp Nr trainable parameters: 10929066
-DECLARE_A3C_AGENT(A3C_LSTM_BMNP)
+// a3c-mq28-tcn Nr trainable parameters: 934434
+// a3c-mq130-tcn Nr trainable parameters: 10845966
+DECLARE_A3C_AGENT(A3C_TCN)
+// a3c-mq28-lstm Nr trainable parameters: 1057986
+// a3c-mq130-lstm Nr trainable parameters: 10160466
+DECLARE_A3C_AGENT(A3C_LSTM)
 // a3c-mq28-hybrid Nr trainable parameters: 860994
 // a3c-mq130-hybrid Nr trainable parameters: 9284466
 DECLARE_A3C_AGENT(A3C_HYBRID)
--- AI/src/NeuralNetworks/Agents/A3C/a3c_agents.cpp
+++ AI/src/NeuralNetworks/Agents/A3C/a3c_agents.cpp
@@ -14,7 +14,7 @@
 #include <sstream>
 
 namespace ai_pass_selector {
-A3C_TCN_RELU::A3C_TCN_RELU(unsigned int max_qubits, bool is_boss)
+A3C_TCN::A3C_TCN(unsigned int max_qubits, bool is_boss)
     : BaseA3CAgent(max_qubits, is_boss) {
   // Nr trainable parameters: unknown
   const unsigned int nr_residual_blocks =
@@ -25,43 +25,18 @@ A3C_TCN_RELU::A3C_TCN_RELU(unsigned int max_qubits, bool is_boss)
       make_TCN_critic(max_qubits, nr_residual_blocks, kernel_size));
 }
 
-A3C_TCN_PRELU::A3C_TCN_PRELU(unsigned int max_qubits, bool is_boss)
-    : BaseA3CAgent(max_qubits, is_boss) {
-  // Nr trainable parameters: unknown
-  const unsigned int nr_residual_blocks =
-      std::ceil(std::log2(TCN_QUBIT_MAGIC * max_qubits));
-  constexpr unsigned int kernel_size = 5u;
-  constexpr double prelu_init = 0.1;
-  this->initialize(
-      make_TCN_actor(max_qubits, nr_residual_blocks, kernel_size, prelu_init),
-      make_TCN_critic(max_qubits, nr_residual_blocks, kernel_size, prelu_init));
-}
-
-A3C_LSTM_HMPP::A3C_LSTM_HMPP(unsigned int max_qubits, bool is_boss)
-    : BaseA3CAgent(max_qubits, is_boss) {
-  constexpr unsigned int hidden_size_multiplier = 8u;
-  constexpr unsigned int projection_size_multiplier = 2u;
-  this->initialize(
-      make_LSTM_actor(max_qubits, hidden_size_multiplier,
-                      projection_size_multiplier),
-      make_LSTM_critic(max_qubits, hidden_size_multiplier,
-                       projection_size_multiplier));
-}
-
-A3C_LSTM_BMNP::A3C_LSTM_BMNP(unsigned int max_qubits, bool is_boss)
+A3C_LSTM::A3C_LSTM(unsigned int max_qubits, bool is_boss)
     : BaseA3CAgent(max_qubits, is_boss) {
   constexpr unsigned int hidden_size_multiplier = 5u;
   constexpr unsigned int projection_size_multiplier = 5u;
-  this->initialize(
-      make_LSTM_actor(max_qubits, hidden_size_multiplier,
-                      projection_size_multiplier),
-      make_LSTM_critic(max_qubits, hidden_size_multiplier,
-                       projection_size_multiplier));
+  this->initialize(make_LSTM_actor(max_qubits, hidden_size_multiplier,
+                                   projection_size_multiplier),
+                   make_LSTM_critic(max_qubits, hidden_size_multiplier,
+                                    projection_size_multiplier));
 }
 
 A3C_HYBRID::A3C_HYBRID(unsigned int max_qubits, bool is_boss)
     : BaseA3CAgent(max_qubits, is_boss) {
-  // Nr trainable parameters: unknown
   const unsigned int nr_residual_blocks =
       std::ceil(std::log2(HYBRID_QUBIT_MAGIC * max_qubits));
   constexpr unsigned int kernel_size = 3u;
@@ -74,51 +49,29 @@ A3C_HYBRID::A3C_HYBRID(unsigned int max_qubits, bool is_boss)
                          hidden_size_multiplier, projection_size_multiplier));
 }
 
-std::unique_ptr<BaseA3CAgent> A3C_TCN_RELU::clone() const {
-  return std::make_unique<A3C_TCN_RELU>(this->max_qubits, /*is_boss=*/false);
+std::unique_ptr<BaseA3CAgent> A3C_TCN::clone() const {
+  return std::make_unique<A3C_TCN>(this->max_qubits, /*is_boss=*/false);
 }
 
-std::unique_ptr<BaseA3CAgent> A3C_TCN_PRELU::clone() const {
-  return std::make_unique<A3C_TCN_PRELU>(this->max_qubits, /*is_boss=*/false);
-}
-
-std::unique_ptr<BaseA3CAgent> A3C_LSTM_HMPP::clone() const {
-  return std::make_unique<A3C_LSTM_HMPP>(this->max_qubits, /*is_boss=*/false);
-}
-
-std::unique_ptr<BaseA3CAgent> A3C_LSTM_BMNP::clone() const {
-  return std::make_unique<A3C_LSTM_BMNP>(this->max_qubits, /*is_boss=*/false);
+std::unique_ptr<BaseA3CAgent> A3C_LSTM::clone() const {
+  return std::make_unique<A3C_LSTM>(this->max_qubits, /*is_boss=*/false);
 }
 
 std::unique_ptr<BaseA3CAgent> A3C_HYBRID::clone() const {
   return std::make_unique<A3C_HYBRID>(this->max_qubits, /*is_boss=*/false);
 }
 
-std::string A3C_TCN_RELU::agentName() const {
+std::string A3C_TCN::agentName() const {
   std::string size_string = "mq" + std::to_string(this->max_qubits);
   std::ostringstream oss;
-  oss << "a3c-" << size_string << "-tcnrelu";
+  oss << "a3c-" << size_string << "-tcn";
   return oss.str();
 }
 
-std::string A3C_TCN_PRELU::agentName() const {
+std::string A3C_LSTM::agentName() const {
   std::string size_string = "mq" + std::to_string(this->max_qubits);
   std::ostringstream oss;
-  oss << "a3c-" << size_string << "-tcnprelu";
-  return oss.str();
-}
-
-std::string A3C_LSTM_HMPP::agentName() const {
-  std::string size_string = "mq" + std::to_string(this->max_qubits);
-  std::ostringstream oss;
-  oss << "a3c-" << size_string << "-lstmhmpp";
-  return oss.str();
-}
-
-std::string A3C_LSTM_BMNP::agentName() const {
-  std::string size_string = "mq" + std::to_string(this->max_qubits);
-  std::ostringstream oss;
-  oss << "a3c-" << size_string << "-lstmbmnp";
+  oss << "a3c-" << size_string << "-lstm";
   return oss.str();
 }
 
