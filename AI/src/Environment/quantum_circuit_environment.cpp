8784b8b338dc1f2d256c1555f23adb1f5719be62
Leonard Uscinowicz <ge78zic@mytum.de>
2025-12-03 12:33:47 +0100

    Fixed bugs and syntax.

--- AI/src/Environment/quantum_circuit_environment.cpp
+++ AI/src/Environment/quantum_circuit_environment.cpp
@@ -113,7 +113,7 @@ void QuantumCircuitEnvironment::reset(std::optional<int> seed) {
     if (seed.has_value()) {
       randomizer_options.seed = seed.value();
     }
-if (this->training
+if (this->training_max_nr_gates > 2u)
     this->circuit = random_quantum_circuit_from_statistics_arrays(
         qubits_cholesky_params.value(), gates_weights.value(),
         randomizer_options);
