83bcba1d018cd11c58bced1e9f07b6ccc98262b0
Leonard Uscinowicz <ge78zic@mytum.de>
2025-12-03 12:34:40 +0100

    Fixed bugs and syntax.

--- AI/include/Environment/quantum_circuit_environment.hpp
+++ AI/include/Environment/quantum_circuit_environment.hpp
@@ -58,6 +58,7 @@ protected:
   /// Attributes for circuit
   std::optional<InstructionsTensor<float>> latest_observation = std::nullopt;
   unsigned int max_qubits = GLOBAL_MIN_NR_QUBITS;
+  unsigned int
   fs::path circuit_path = "";
   QuantumCircuit circuit{};
 
